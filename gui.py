38cd5d3624446e71fb2f140b773b43191c201b21
Ridvik Pal <pal.ridvik@gmail.com>
2023-06-25 23:48:24 -0400

    changed up the gui again.
    this time made it more simple. Will add more complex features later on.

--- gui.py
+++ gui.py
@@ -14,18 +14,18 @@ import ui_user_interface_design
 
 ''' FUNCTION DEFINITIONS '''
 # show the plotly figure in a seperate window using QT web engine
-def show_in_window(fig, name: str):
-    # update the figure title with the company symbol
-    fig.update_layout(title_text=name, title_x=0.5, title_font_size=20)
+# def show_in_window(fig, name: str):
+#     # update the figure title with the company symbol
+#     fig.update_layout(title_text=name, title_x=0.5, title_font_size=20)
 
-    plotly.offline.plot(fig, filename='figure.html', auto_open=False)
+#     plotly.offline.plot(fig, filename='figure.html', auto_open=False)
 
-    app = QApplication(sys.argv)
-    web = QWebEngineView()
-    file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "figure.html"))
-    web.load(QUrl.fromLocalFile(file_path))
-    web.show()
-    sys.exit(app.exec_())
+#     app = QApplication(sys.argv)
+#     web = QWebEngineView()
+#     file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "figure.html"))
+#     web.load(QUrl.fromLocalFile(file_path))
+#     web.show()
+#     sys.exit(app.exec_())
 
 def createMainWindow() -> None:
     app = ui_user_interface_design.QApplication(sys.argv)
--- ui_user_interface_design.py
+++ ui_user_interface_design.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'user_interface_designheHNiq.ui'
+## Form generated from reading UI file 'user_interface_designcJnntN.ui'
 ##
 ## Created by: Qt User Interface Compiler version 5.15.2
 ##
@@ -19,174 +19,151 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(1084, 690)
+        MainWindow.resize(1411, 794)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        self.currentPriceGroup = QGroupBox(self.centralwidget)
+        self.currentPriceGroup.setObjectName(u"currentPriceGroup")
+        self.currentPriceGroup.setGeometry(QRect(1100, 270, 301, 221))
+        self.currentPriceGroup.setCheckable(False)
+        self.currentPriceTable = QTableWidget(self.currentPriceGroup)
+        self.currentPriceTable.setObjectName(u"currentPriceTable")
+        self.currentPriceTable.setGeometry(QRect(10, 20, 281, 191))
+        font = QFont()
+        font.setFamily(u"Arial")
+        self.currentPriceTable.setFont(font)
+        self.fiftyTwoWeekAverageGroup = QGroupBox(self.centralwidget)
+        self.fiftyTwoWeekAverageGroup.setObjectName(u"fiftyTwoWeekAverageGroup")
+        self.fiftyTwoWeekAverageGroup.setGeometry(QRect(1100, 500, 301, 251))
+        self.fiftyTwoWeekAverageGroup.setCheckable(False)
+        self.fiftyTwoWeekAverageTable = QTableWidget(self.fiftyTwoWeekAverageGroup)
+        self.fiftyTwoWeekAverageTable.setObjectName(u"fiftyTwoWeekAverageTable")
+        self.fiftyTwoWeekAverageTable.setGeometry(QRect(10, 20, 281, 221))
+        self.fiftyTwoWeekAverageTable.setFont(font)
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(1100, 19, 301, 31))
+        font1 = QFont()
+        font1.setFamily(u"Arial")
+        font1.setPointSize(14)
+        self.lineEdit.setFont(font1)
         self.layoutWidget = QWidget(self.centralwidget)
         self.layoutWidget.setObjectName(u"layoutWidget")
-        self.layoutWidget.setGeometry(QRect(0, 0, 1081, 641))
-        self.horizontalLayout = QHBoxLayout(self.layoutWidget)
+        self.layoutWidget.setGeometry(QRect(1100, 60, 301, 52))
+        self.horizontalLayout_2 = QHBoxLayout(self.layoutWidget)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.stockSymbol = QLabel(self.layoutWidget)
+        self.stockSymbol.setObjectName(u"stockSymbol")
+        font2 = QFont()
+        font2.setFamily(u"Arial")
+        font2.setPointSize(32)
+        self.stockSymbol.setFont(font2)
+
+        self.horizontalLayout_2.addWidget(self.stockSymbol)
+
+        self.stockName = QLabel(self.layoutWidget)
+        self.stockName.setObjectName(u"stockName")
+        font3 = QFont()
+        font3.setPointSize(14)
+        self.stockName.setFont(font3)
+        self.stockName.setFrameShape(QFrame.NoFrame)
+        self.stockName.setFrameShadow(QFrame.Plain)
+        self.stockName.setAlignment(Qt.AlignBottom|Qt.AlignHCenter)
+
+        self.horizontalLayout_2.addWidget(self.stockName)
+
+        self.layoutWidget1 = QWidget(self.centralwidget)
+        self.layoutWidget1.setObjectName(u"layoutWidget1")
+        self.layoutWidget1.setGeometry(QRect(1100, 120, 301, 141))
+        self.horizontalLayout = QHBoxLayout(self.layoutWidget1)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.stockImage = QLabel(self.layoutWidget1)
+        self.stockImage.setObjectName(u"stockImage")
 
-        # self.plotlyGraph = QWidget(self.layoutWidget)
-        self.plotlyGraph = QWebEngineView(self.layoutWidget)
+        self.horizontalLayout.addWidget(self.stockImage)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.stockExchange = QLabel(self.layoutWidget1)
+        self.stockExchange.setObjectName(u"stockExchange")
+        self.stockExchange.setFont(font3)
+        self.stockExchange.setFrameShape(QFrame.NoFrame)
+        self.stockExchange.setFrameShadow(QFrame.Plain)
+        self.stockExchange.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.stockExchange)
+
+        self.stockMicCode = QLabel(self.layoutWidget1)
+        self.stockMicCode.setObjectName(u"stockMicCode")
+        self.stockMicCode.setFont(font3)
+        self.stockMicCode.setFrameShape(QFrame.NoFrame)
+        self.stockMicCode.setFrameShadow(QFrame.Plain)
+        self.stockMicCode.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.stockMicCode)
+
+        self.stockCurrency = QLabel(self.layoutWidget1)
+        self.stockCurrency.setObjectName(u"stockCurrency")
+        self.stockCurrency.setFont(font3)
+        self.stockCurrency.setFrameShape(QFrame.NoFrame)
+        self.stockCurrency.setFrameShadow(QFrame.Plain)
+        self.stockCurrency.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.stockCurrency)
+
+        self.stockTimeStamp = QLabel(self.layoutWidget1)
+        self.stockTimeStamp.setObjectName(u"stockTimeStamp")
+        self.stockTimeStamp.setFont(font3)
+        self.stockTimeStamp.setFrameShape(QFrame.NoFrame)
+        self.stockTimeStamp.setFrameShadow(QFrame.Plain)
+        self.stockTimeStamp.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.stockTimeStamp)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.plotlyGroupBox = QGroupBox(self.centralwidget)
+        self.plotlyGroupBox.setObjectName(u"plotlyGroupBox")
+        self.plotlyGroupBox.setGeometry(QRect(10, 10, 1081, 741))
+
+        # self.plotlyGraph = QWidget(self.plotlyGroupBox)
+        # self.plotlyGraph.setObjectName(u"plotlyGraph")
+        # self.plotlyGraph.setGeometry(QRect(10, 20, 1061, 711))
+
+        # setup the plotly graph
+        self.plotlyGraph = QWebEngineView(self.plotlyGroupBox)
         file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "figure.html"))
         self.plotlyGraph.load(QUrl.fromLocalFile(file_path))
-
-
         self.plotlyGraph.setObjectName(u"plotlyGraph")
+        self.plotlyGraph.setGeometry(QRect(10, 20, 1061, 711))
 
-        self.horizontalLayout.addWidget(self.plotlyGraph)
-
-        self.tabs = QTabWidget(self.layoutWidget)
-        self.tabs.setObjectName(u"tabs")
-        self.stockTab = QWidget()
-        self.stockTab.setObjectName(u"stockTab")
-        self.stockInput = QLineEdit(self.stockTab)
-        self.stockInput.setObjectName(u"stockInput")
-        self.stockInput.setGeometry(QRect(10, 10, 331, 20))
-        self.stockSymbol = QLabel(self.stockTab)
-        self.stockSymbol.setObjectName(u"stockSymbol")
-        self.stockSymbol.setGeometry(QRect(10, 40, 331, 31))
-        font = QFont()
-        font.setPointSize(20)
-        font.setItalic(False)
-        self.stockSymbol.setFont(font)
-        self.stockSymbol.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
-        self.formLayoutWidget = QWidget(self.stockTab)
-        self.formLayoutWidget.setObjectName(u"formLayoutWidget")
-        self.formLayoutWidget.setGeometry(QRect(10, 80, 331, 141))
-        self.formLayout = QFormLayout(self.formLayoutWidget)
-        self.formLayout.setObjectName(u"formLayout")
-        self.formLayout.setContentsMargins(0, 0, 0, 0)
-        self.stockNameLabel = QLabel(self.formLayoutWidget)
-        self.stockNameLabel.setObjectName(u"stockNameLabel")
-        font1 = QFont()
-        font1.setPointSize(14)
-        font1.setBold(True)
-        font1.setWeight(75)
-        self.stockNameLabel.setFont(font1)
-
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.stockNameLabel)
-
-        self.stockExchangeLabel = QLabel(self.formLayoutWidget)
-        self.stockExchangeLabel.setObjectName(u"stockExchangeLabel")
-        self.stockExchangeLabel.setFont(font1)
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.stockExchangeLabel)
-
-        self.stockMicCodeLabel = QLabel(self.formLayoutWidget)
-        self.stockMicCodeLabel.setObjectName(u"stockMicCodeLabel")
-        self.stockMicCodeLabel.setFont(font1)
-
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.stockMicCodeLabel)
-
-        self.stockCurrencyLabel = QLabel(self.formLayoutWidget)
-        self.stockCurrencyLabel.setObjectName(u"stockCurrencyLabel")
-        self.stockCurrencyLabel.setFont(font1)
-
-        self.formLayout.setWidget(3, QFormLayout.LabelRole, self.stockCurrencyLabel)
-
-        self.stockTimeStampLabel = QLabel(self.formLayoutWidget)
-        self.stockTimeStampLabel.setObjectName(u"stockTimeStampLabel")
-        self.stockTimeStampLabel.setFont(font1)
-
-        self.formLayout.setWidget(4, QFormLayout.LabelRole, self.stockTimeStampLabel)
-
-        self.stockName = QLabel(self.formLayoutWidget)
-        self.stockName.setObjectName(u"stockName")
-        font2 = QFont()
-        font2.setPointSize(14)
-        self.stockName.setFont(font2)
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.stockName)
-
-        self.stockExchange = QLabel(self.formLayoutWidget)
-        self.stockExchange.setObjectName(u"stockExchange")
-        self.stockExchange.setFont(font2)
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.stockExchange)
-
-        self.stockMicCode = QLabel(self.formLayoutWidget)
-        self.stockMicCode.setObjectName(u"stockMicCode")
-        self.stockMicCode.setFont(font2)
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.stockMicCode)
-
-        self.stockCurrency = QLabel(self.formLayoutWidget)
-        self.stockCurrency.setObjectName(u"stockCurrency")
-        self.stockCurrency.setFont(font2)
-
-        self.formLayout.setWidget(3, QFormLayout.FieldRole, self.stockCurrency)
-
-        self.stockTimeStamp = QLabel(self.formLayoutWidget)
-        self.stockTimeStamp.setObjectName(u"stockTimeStamp")
-        self.stockTimeStamp.setFont(font2)
-
-        self.formLayout.setWidget(4, QFormLayout.FieldRole, self.stockTimeStamp)
-
-        self.currentPriceTable = QTableView(self.stockTab)
-        self.currentPriceTable.setObjectName(u"currentPriceTable")
-        self.currentPriceTable.setGeometry(QRect(10, 240, 331, 81))
-        self.fiftyTwoWeekAverageLabel = QLabel(self.stockTab)
-        self.fiftyTwoWeekAverageLabel.setObjectName(u"fiftyTwoWeekAverageLabel")
-        self.fiftyTwoWeekAverageLabel.setGeometry(QRect(10, 330, 331, 21))
-        font3 = QFont()
-        font3.setPointSize(12)
-        self.fiftyTwoWeekAverageLabel.setFont(font3)
-        self.fiftyTwoWeekAverageTable = QTableView(self.stockTab)
-        self.fiftyTwoWeekAverageTable.setObjectName(u"fiftyTwoWeekAverageTable")
-        self.fiftyTwoWeekAverageTable.setGeometry(QRect(10, 360, 331, 241))
-        self.tabs.addTab(self.stockTab, "")
-        self.etfTab = QWidget()
-        self.etfTab.setObjectName(u"etfTab")
-        self.tabs.addTab(self.etfTab, "")
-
-        self.horizontalLayout.addWidget(self.tabs)
-
-        self.horizontalLayout.setStretch(0, 2)
-        self.horizontalLayout.setStretch(1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 1084, 21))
+        self.menubar.setGeometry(QRect(0, 0, 1411, 21))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
-        # # extension for plotly web widget
-        # self.web = QWebEngineView()
-        # file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "figure.html"))
-        # self.web.load(QUrl.fromLocalFile(file_path))
-        # self.web.show()
-        # ##
-
         self.retranslateUi(MainWindow)
-
-        self.tabs.setCurrentIndex(0)
-
-
-
 
         QMetaObject.connectSlotsByName(MainWindow)
     # setupUi
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.stockSymbol.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.stockNameLabel.setText(QCoreApplication.translate("MainWindow", u"Name", None))
-        self.stockExchangeLabel.setText(QCoreApplication.translate("MainWindow", u"Exchange", None))
-        self.stockMicCodeLabel.setText(QCoreApplication.translate("MainWindow", u"Mic Code", None))
-        self.stockCurrencyLabel.setText(QCoreApplication.translate("MainWindow", u"Currency", None))
-        self.stockTimeStampLabel.setText(QCoreApplication.translate("MainWindow", u"Time Stamp", None))
-        self.stockName.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.stockExchange.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.stockMicCode.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.stockCurrency.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.stockTimeStamp.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.fiftyTwoWeekAverageLabel.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
-        self.tabs.setTabText(self.tabs.indexOf(self.stockTab), QCoreApplication.translate("MainWindow", u"Stocks", None))
-        self.tabs.setTabText(self.tabs.indexOf(self.etfTab), QCoreApplication.translate("MainWindow", u"ETFs", None))
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MarkedUp", None))
+        self.currentPriceGroup.setTitle(QCoreApplication.translate("MainWindow", u"Current Prices", None))
+        self.fiftyTwoWeekAverageGroup.setTitle(QCoreApplication.translate("MainWindow", u"52-Week Average", None))
+        self.stockSymbol.setText("")
+        self.stockName.setText("")
+        self.stockImage.setText("")
+        self.stockExchange.setText("")
+        self.stockMicCode.setText("")
+        self.stockCurrency.setText("")
+        self.stockTimeStamp.setText("")
+        self.plotlyGroupBox.setTitle(QCoreApplication.translate("MainWindow", u"CandleStick Chart", None))
     # retranslateUi
