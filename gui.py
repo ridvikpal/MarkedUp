e3f1b38f5e33c1d012998ed0be2b335181545080
Ridvik Pal <pal.ridvik@gmail.com>
2023-06-27 16:04:23 -0400

    ability to get all stock data and update the data table

--- gui.py
+++ gui.py
@@ -15,6 +15,7 @@ from PyQt5.QtWebEngineWidgets import QWebEngineView
 import os
 import sys
 import connection
+from datetime import datetime
 import pandas
 
 
@@ -32,6 +33,7 @@ class Ui_MainWindow(object):
         # self.plotlyGraph = QWidget(self.plotlyGroup)
         # self.plotlyGraph.setObjectName(u"plotlyGraph")
         # self.plotlyGraph.setGeometry(QRect(10, 20, 1051, 781))
+
         ### Setup plotly graph
         self.plotlyGraph = QWebEngineView(self.plotlyGroup)
         file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "figure.html"))
@@ -56,50 +58,50 @@ class Ui_MainWindow(object):
         self.stockDataGroup = QGroupBox(self.centralwidget)
         self.stockDataGroup.setObjectName(u"stockDataGroup")
         self.stockDataGroup.setGeometry(QRect(1090, 80, 321, 511))
-        self.currentPriceTable = QTableWidget(self.stockDataGroup)
-        if (self.currentPriceTable.columnCount() < 1):
-            self.currentPriceTable.setColumnCount(1)
+        self.stockDataTable = QTableWidget(self.stockDataGroup)
+        if (self.stockDataTable.columnCount() < 1):
+            self.stockDataTable.setColumnCount(1)
         __qtablewidgetitem = QTableWidgetItem()
-        self.currentPriceTable.setHorizontalHeaderItem(0, __qtablewidgetitem)
-        if (self.currentPriceTable.rowCount() < 14):
-            self.currentPriceTable.setRowCount(14)
+        self.stockDataTable.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        if (self.stockDataTable.rowCount() < 14):
+            self.stockDataTable.setRowCount(14)
         __qtablewidgetitem1 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(0, __qtablewidgetitem1)
+        self.stockDataTable.setVerticalHeaderItem(0, __qtablewidgetitem1)
         __qtablewidgetitem2 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(1, __qtablewidgetitem2)
+        self.stockDataTable.setVerticalHeaderItem(1, __qtablewidgetitem2)
         __qtablewidgetitem3 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(2, __qtablewidgetitem3)
+        self.stockDataTable.setVerticalHeaderItem(2, __qtablewidgetitem3)
         __qtablewidgetitem4 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(3, __qtablewidgetitem4)
+        self.stockDataTable.setVerticalHeaderItem(3, __qtablewidgetitem4)
         __qtablewidgetitem5 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(4, __qtablewidgetitem5)
+        self.stockDataTable.setVerticalHeaderItem(4, __qtablewidgetitem5)
         __qtablewidgetitem6 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(5, __qtablewidgetitem6)
+        self.stockDataTable.setVerticalHeaderItem(5, __qtablewidgetitem6)
         __qtablewidgetitem7 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(6, __qtablewidgetitem7)
+        self.stockDataTable.setVerticalHeaderItem(6, __qtablewidgetitem7)
         __qtablewidgetitem8 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(7, __qtablewidgetitem8)
+        self.stockDataTable.setVerticalHeaderItem(7, __qtablewidgetitem8)
         __qtablewidgetitem9 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(8, __qtablewidgetitem9)
+        self.stockDataTable.setVerticalHeaderItem(8, __qtablewidgetitem9)
         __qtablewidgetitem10 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(9, __qtablewidgetitem10)
+        self.stockDataTable.setVerticalHeaderItem(9, __qtablewidgetitem10)
         __qtablewidgetitem11 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(10, __qtablewidgetitem11)
+        self.stockDataTable.setVerticalHeaderItem(10, __qtablewidgetitem11)
         __qtablewidgetitem12 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(11, __qtablewidgetitem12)
+        self.stockDataTable.setVerticalHeaderItem(11, __qtablewidgetitem12)
         __qtablewidgetitem13 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(12, __qtablewidgetitem13)
+        self.stockDataTable.setVerticalHeaderItem(12, __qtablewidgetitem13)
         __qtablewidgetitem14 = QTableWidgetItem()
-        self.currentPriceTable.setVerticalHeaderItem(13, __qtablewidgetitem14)
-        self.currentPriceTable.setObjectName(u"currentPriceTable")
-        self.currentPriceTable.setGeometry(QRect(10, 150, 301, 351))
+        self.stockDataTable.setVerticalHeaderItem(13, __qtablewidgetitem14)
+        self.stockDataTable.setObjectName(u"stockDataTable")
+        self.stockDataTable.setGeometry(QRect(10, 150, 301, 351))
         font1 = QFont()
         font1.setFamily(u"Arial")
-        self.currentPriceTable.setFont(font1)
-        self.currentPriceTable.horizontalHeader().setCascadingSectionResizes(False)
-        self.currentPriceTable.horizontalHeader().setDefaultSectionSize(200)
-        self.currentPriceTable.verticalHeader().setMinimumSectionSize(23)
-        self.currentPriceTable.verticalHeader().setDefaultSectionSize(23)
+        self.stockDataTable.setFont(font1)
+        self.stockDataTable.horizontalHeader().setCascadingSectionResizes(False)
+        self.stockDataTable.horizontalHeader().setDefaultSectionSize(200)
+        self.stockDataTable.verticalHeader().setMinimumSectionSize(23)
+        self.stockDataTable.verticalHeader().setDefaultSectionSize(23)
         self.widget = QWidget(self.stockDataGroup)
         self.widget.setObjectName(u"widget")
         self.widget.setGeometry(QRect(10, 20, 301, 121))
@@ -153,35 +155,35 @@ class Ui_MainWindow(object):
         self.plotlyGroup.setTitle(QCoreApplication.translate("MainWindow", u"CandleStick Chart", None))
         self.stockSearchGroup.setTitle(QCoreApplication.translate("MainWindow", u"Stock Search", None))
         self.stockDataGroup.setTitle(QCoreApplication.translate("MainWindow", u"Stock Data", None))
-        ___qtablewidgetitem = self.currentPriceTable.horizontalHeaderItem(0)
+        ___qtablewidgetitem = self.stockDataTable.horizontalHeaderItem(0)
         ___qtablewidgetitem.setText(QCoreApplication.translate("MainWindow", u"Data", None));
-        ___qtablewidgetitem1 = self.currentPriceTable.verticalHeaderItem(0)
+        ___qtablewidgetitem1 = self.stockDataTable.verticalHeaderItem(0)
         ___qtablewidgetitem1.setText(QCoreApplication.translate("MainWindow", u"Symbol", None));
-        ___qtablewidgetitem2 = self.currentPriceTable.verticalHeaderItem(1)
+        ___qtablewidgetitem2 = self.stockDataTable.verticalHeaderItem(1)
         ___qtablewidgetitem2.setText(QCoreApplication.translate("MainWindow", u"Exchange", None));
-        ___qtablewidgetitem3 = self.currentPriceTable.verticalHeaderItem(2)
+        ___qtablewidgetitem3 = self.stockDataTable.verticalHeaderItem(2)
         ___qtablewidgetitem3.setText(QCoreApplication.translate("MainWindow", u"MIC", None));
-        ___qtablewidgetitem4 = self.currentPriceTable.verticalHeaderItem(3)
+        ___qtablewidgetitem4 = self.stockDataTable.verticalHeaderItem(3)
         ___qtablewidgetitem4.setText(QCoreApplication.translate("MainWindow", u"Currency", None));
-        ___qtablewidgetitem5 = self.currentPriceTable.verticalHeaderItem(4)
+        ___qtablewidgetitem5 = self.stockDataTable.verticalHeaderItem(4)
         ___qtablewidgetitem5.setText(QCoreApplication.translate("MainWindow", u"TimeStamp", None));
-        ___qtablewidgetitem6 = self.currentPriceTable.verticalHeaderItem(5)
+        ___qtablewidgetitem6 = self.stockDataTable.verticalHeaderItem(5)
         ___qtablewidgetitem6.setText(QCoreApplication.translate("MainWindow", u"Open", None));
-        ___qtablewidgetitem7 = self.currentPriceTable.verticalHeaderItem(6)
+        ___qtablewidgetitem7 = self.stockDataTable.verticalHeaderItem(6)
         ___qtablewidgetitem7.setText(QCoreApplication.translate("MainWindow", u"High", None));
-        ___qtablewidgetitem8 = self.currentPriceTable.verticalHeaderItem(7)
+        ___qtablewidgetitem8 = self.stockDataTable.verticalHeaderItem(7)
         ___qtablewidgetitem8.setText(QCoreApplication.translate("MainWindow", u"Low", None));
-        ___qtablewidgetitem9 = self.currentPriceTable.verticalHeaderItem(8)
+        ___qtablewidgetitem9 = self.stockDataTable.verticalHeaderItem(8)
         ___qtablewidgetitem9.setText(QCoreApplication.translate("MainWindow", u"Close", None));
-        ___qtablewidgetitem10 = self.currentPriceTable.verticalHeaderItem(9)
+        ___qtablewidgetitem10 = self.stockDataTable.verticalHeaderItem(9)
         ___qtablewidgetitem10.setText(QCoreApplication.translate("MainWindow", u"Volume", None));
-        ___qtablewidgetitem11 = self.currentPriceTable.verticalHeaderItem(10)
+        ___qtablewidgetitem11 = self.stockDataTable.verticalHeaderItem(10)
         ___qtablewidgetitem11.setText(QCoreApplication.translate("MainWindow", u"Previous Close", None));
-        ___qtablewidgetitem12 = self.currentPriceTable.verticalHeaderItem(11)
+        ___qtablewidgetitem12 = self.stockDataTable.verticalHeaderItem(11)
         ___qtablewidgetitem12.setText(QCoreApplication.translate("MainWindow", u"Change", None));
-        ___qtablewidgetitem13 = self.currentPriceTable.verticalHeaderItem(12)
+        ___qtablewidgetitem13 = self.stockDataTable.verticalHeaderItem(12)
         ___qtablewidgetitem13.setText(QCoreApplication.translate("MainWindow", u"Percent Change", None));
-        ___qtablewidgetitem14 = self.currentPriceTable.verticalHeaderItem(13)
+        ___qtablewidgetitem14 = self.stockDataTable.verticalHeaderItem(13)
         ___qtablewidgetitem14.setText(QCoreApplication.translate("MainWindow", u"Average Volume", None));
         self.stockImage.setText("")
         self.stockName.setText("")
@@ -198,22 +200,25 @@ class Ui_MainWindow(object):
         stockSymbol = self.stockSearch.text()
         quote = connection.getStockQuote(stockSymbol)
 
-        # # update stock symbol
-        # self.stockSymbol.setText(stockSymbol)
-        # self.stockSymbol.adjustSize()
-        # # update the stock name
-        # self.stockName.setText(str(quote['name'][0]))
-        # # update the stock
-        # self.stockExchange.setText(str(quote['exchange'][0]))
-        # # update the stock mic code
-        # self.stockMicCode.setText(str(quote['mic_code'][0]))
-        # # update the currency
-        # self.stockCurrency.setText(str(quote['currency'][0]))
-        # # update the timestamp
-        # self.stockTimeStamp.setText(str(quote['timestamp'][0]))
+        # update the data table
+        self.stockDataTable.setItem(0, 0, QTableWidgetItem(str(quote['symbol'][0])))
+        self.stockDataTable.setItem(1, 0, QTableWidgetItem(str(quote['exchange'][0])))
+        self.stockDataTable.setItem(2, 0, QTableWidgetItem(str(quote['mic_code'][0])))
+        self.stockDataTable.setItem(3, 0, QTableWidgetItem(str(quote['currency'][0])))
+        self.stockDataTable.setItem(4, 0, QTableWidgetItem(datetime.utcfromtimestamp(quote['timestamp'][0]).strftime('%Y-%m-%d %H:%M:%S')))
+        self.stockDataTable.setItem(5, 0, QTableWidgetItem(str(quote['open'][0])))
+        self.stockDataTable.setItem(6, 0, QTableWidgetItem(str(quote['high'][0])))
+        self.stockDataTable.setItem(7, 0, QTableWidgetItem(str(quote['low'][0])))
+        self.stockDataTable.setItem(8, 0, QTableWidgetItem(str(quote['close'][0])))
+        self.stockDataTable.setItem(9, 0, QTableWidgetItem(str(quote['volume'][0])))
+        self.stockDataTable.setItem(10, 0, QTableWidgetItem(str(quote['previous_close'][0])))
+        self.stockDataTable.setItem(11, 0, QTableWidgetItem(str(quote['change'][0])))
+        self.stockDataTable.setItem(12, 0, QTableWidgetItem(str(quote['percent_change'][0]) + "%"))
+        self.stockDataTable.setItem(13, 0, QTableWidgetItem(str(quote['average_volume'][0])))
 
-        # update the table
-        # self.currentPriceTable.
+        # update the stock name
+        self.stockName.setText(str(quote['name'][0]))
+
 
 def createMainWindow() -> None:
     app = QApplication(sys.argv)
@@ -222,3 +227,7 @@ def createMainWindow() -> None:
     ui.setupUi(window)
     window.show()
     sys.exit(app.exec_())
+
+def testFunction():
+    quote = connection.getStockQuote("AAPL")
+    quote.drop(columns=['fifty_two_week', 'is_market_open'], axis=1, inplace=True)
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import connection
 ''' DEFINE MAIN FUNCTION '''
 def main() -> None:
     gui.createMainWindow()
+    # gui.testFunction()
     # test = connection.getStockQuote("AAPL")
     # fiftyTwoWeek = test['fifty_two_week'][0]
     # print(fiftyTwoWeek)
